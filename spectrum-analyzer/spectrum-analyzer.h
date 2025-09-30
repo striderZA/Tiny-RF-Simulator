4cfc5d51a57cc7b5dd554f81032060807ea3de47
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-09-30 15:46:15 +0200

    Add start/stop frequency members

--- spectrum-analyzer/spectrum-analyzer.h
+++ spectrum-analyzer/spectrum-analyzer.h
@@ -10,7 +10,14 @@ public:
 	SpectrumAnalyzer();
 	void analyze(const Spectrum& input);
 	void draw(const char* title, bool* p_open);
+
+	void setStartFrequency(double start_freq) { m_start_freq = start_freq; }
+	void setStopFrequency(double stop_freq) { m_stop_freq = stop_freq; }
+
 private:
 	Spectrum m_current_spectrum;
 	bool m_show_spectrum = false;
+
+	double m_start_freq = MIN_FREQ;
+	double m_stop_freq = MAX_FREQ;
 };
