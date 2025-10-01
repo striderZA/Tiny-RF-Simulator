125809d3c7473b5abc59de0e4a3a44e4e58020b5
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-10-01 08:37:24 +0200

    Add min/max power and additional getters

--- spectrum-analyzer/spectrum-analyzer.h
+++ spectrum-analyzer/spectrum-analyzer.h
@@ -13,6 +13,13 @@ public:
 
 	void setStartFrequency(double start_freq) { m_start_freq = start_freq; }
 	void setStopFrequency(double stop_freq) { m_stop_freq = stop_freq; }
+	void setMinPower(double min_power) { m_min_power = min_power; }
+	void setMaxPower(double max_power) { m_max_power = max_power; }
+
+	double startFrequency() const { return m_start_freq; }
+	double stopFrequency() const { return m_stop_freq; }
+	double minPower() const { return m_min_power; }
+	double maxPower() const { return m_max_power; }
 
 private:
 	Spectrum m_current_spectrum;
@@ -20,4 +27,6 @@ private:
 
 	double m_start_freq = MIN_FREQ;
 	double m_stop_freq = MAX_FREQ;
+	double m_min_power = MIN_POWER;
+	double m_max_power = MAX_POWER;
 };
