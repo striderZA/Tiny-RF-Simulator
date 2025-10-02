a9b0686fe9ca058c90b57cbd5adbac3823cd312d
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-10-02 14:09:45 +0200

    Update frequency limits

--- common/common.h
+++ common/common.h
@@ -9,8 +9,8 @@ struct Spectrum {
 };
 
 // Shared constants (optional, can be moved to specific classes if preferred)
-constexpr double MIN_FREQ = -100;
-constexpr double MAX_FREQ = 100.0;
+constexpr double MIN_FREQ = -5000;
+constexpr double MAX_FREQ = 5000;
 constexpr size_t NUM_BINS = 1024;
 constexpr double MIN_POWER = -174;
 constexpr double MAX_POWER = 10;
