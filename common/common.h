a0f4bad6f5db768909d16ea0db8b35a09d098447
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-10-14 09:23:53 +0200

    Change default frequency range and video BW

--- common/common.h
+++ common/common.h
@@ -9,12 +9,12 @@ struct Spectrum {
 };
 
 // Shared constants (optional, can be moved to specific classes if preferred)
-constexpr double MIN_FREQ = -20e9;
-constexpr double MAX_FREQ = 20e9;
+constexpr double MIN_FREQ = -5.12e9;
+constexpr double MAX_FREQ = 5.12e9;
 constexpr size_t NUM_BINS = 1024;
 constexpr double MIN_POWER = -174;
 constexpr double MAX_POWER = 10;
-constexpr double DEFAULT_VBW = 10e6;
+constexpr double DEFAULT_VBW = 25e6;
 constexpr double DEFAULT_RBW = 50e6;
 constexpr double k = 1.3806e-23;
 constexpr double T = 290.0;
