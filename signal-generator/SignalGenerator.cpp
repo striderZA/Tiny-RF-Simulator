2a6a90c90ce821c9a63761ef3cd42be21cc52c70
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-10-15 13:33:17 +0200

    Clean out SignalGenerator module for rework

--- signal-generator/SignalGenerator.cpp
+++ signal-generator/SignalGenerator.cpp
@@ -1,29 +1,3 @@
 #include "SignalGenerator.h"
-#include <algorithm>
-#include <cmath>
 
-SignalGenerator::SignalGenerator(int id, float freq, float amp, float noise_floor) : m_id(id), m_freq(freq), m_amp(amp), m_noise_floor(noise_floor) {}
-
-void SignalGenerator::update(const char* title, bool* p_open)
-{
-}
-
-void SignalGenerator::initialize()
-{
-}
-
-void SignalGenerator::setFrequency(float freq) {
-	m_freq = freq;
-}
-
-void SignalGenerator::setAmplitude(float amp) {
-	m_amp = amp;
-}
-
-void SignalGenerator::setNoiseFloor(float noise_floor) {
-	m_noise_floor = noise_floor;
-}
-
-void SignalGenerator::update(const Spectrum& input, Spectrum& output)
-{
-}
+SignalGenerator::SignalGenerator() : {}
--- signal-generator/SignalGenerator.h
+++ signal-generator/SignalGenerator.h
@@ -1,29 +1,11 @@
 #pragma once
 
-#include "logging.h"
-#include "common.h"
-#include <vector>
-
 class SignalGenerator {
 public:
-	SignalGenerator(int id, float freq = 1.0f, float amp = -30.0f, float noise_floor = -70.0f);
-
-	void update(const char* title, bool* p_open);
-	void initialize();
-
-	void setFrequency(float freq);
-	void setAmplitude(float amp);
-	void setNoiseFloor(float noise_floor);
-	void update(const Spectrum& input, Spectrum& output);
+	SignalGenerator();
 
 	int id() const { return m_id; }
-	float freq() const { return m_freq; }
-	float amp() const { return m_amp; }
-	float noise() const { return m_noise_floor; }
 
 private:
 	int m_id;
-	float m_freq;
-	float m_amp;
-	float m_noise_floor;
 };
