a67c392d1503b2cb7b7facf99618a296c9babf73
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-10-15 13:46:28 +0200

    Add helpers to update frequency tone

--- signal-generator/SignalGenerator.h
+++ signal-generator/SignalGenerator.h
@@ -1,9 +1,13 @@
 #pragma once
+#include "common.h"
 #include "logging.h"
 
 class SignalGenerator {
 public:
 	SignalGenerator(int id);
+
+	void updateTone(tone input_tone);
+	void setupTone(const char* title, bool* p_open);
 
 	int id() const { return m_id; }
 
