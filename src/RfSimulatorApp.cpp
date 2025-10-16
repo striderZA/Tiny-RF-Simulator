e488cb0c4240ed913da68438823c4de6e7fe51b4
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-10-16 09:11:41 +0200

    Refactor signal generator instantiation and updating

--- src/RfSimulatorApp.cpp
+++ src/RfSimulatorApp.cpp
@@ -6,7 +6,8 @@
 
 RfSimulatorApp::RfSimulatorApp() : m_spectrum_analyzer(), m_signal_generators() {
 	LOG_INFO("Application initialized!");
-	m_signal_generators.push_back(std::make_unique<SignalGenerator>(0));
+	m_signal_generators.push_back(std::make_unique<SignalGenerator>(static_cast<int>(InputSignals::G0)));
+	m_signal_generators.push_back(std::make_unique<SignalGenerator>(static_cast<int>(InputSignals::G1)));
 }
 
 void RfSimulatorApp::onGui() {
@@ -17,13 +18,14 @@ void RfSimulatorApp::onGui() {
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
 
-	for (auto& gen : m_signal_generators) {
-		gen->setupTone("SigGen", nullptr);
-	}
-
 	m_spectrum_analyzer.update("Spectrum", nullptr);
 
 	this->drawSignalGenerators("Signal Generators", nullptr);
+
+	for (auto& gen : m_signal_generators) {
+		std::string title = "Generator " + std::to_string(gen->id());
+		gen->setup(title.c_str(), nullptr);
+	}
 }
 
 void RfSimulatorApp::drawSignalGenerators(const char* title, bool* p_open) {
