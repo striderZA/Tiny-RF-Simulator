bb4b5372d6e9780584c05a345715680bd0d1dec7
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-10-21 15:34:19 +0200

    Add one more signal generator

--- app/src/app.cpp
+++ app/src/app.cpp
@@ -9,6 +9,8 @@ RfSimulatorApp::RfSimulatorApp()
 	: m_spectrum_analyzer(), m_signal_generators(), m_enable_log(true) {
 	m_signal_generators.push_back(
 		std::make_unique<SignalGenerator>(static_cast<int>(InputSignals::G0)));
+	m_signal_generators.push_back(
+		std::make_unique<SignalGenerator>(static_cast<int>(InputSignals::G1)));
 }
 
 void RfSimulatorApp::onGui() {
