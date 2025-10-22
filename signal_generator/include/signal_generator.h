c06ddedf4aa193fff214b1cb0211991b342f27aa
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-10-22 10:07:13 +0200

    Add checkbox to enable measuring the generator output

--- signal_generator/include/signal_generator.h
+++ signal_generator/include/signal_generator.h
@@ -9,8 +9,10 @@ public:
 
 	int id() const { return m_id; }
 	tone activeTone() const { return m_active_tone; }
+	bool measurementActive() const { return m_measurement_active; }
 
 private:
 	int m_id;
+	bool m_measurement_active;
 	tone m_active_tone;
 };
--- signal_generator/src/signal_generator.cpp
+++ signal_generator/src/signal_generator.cpp
@@ -8,6 +8,7 @@ SignalGenerator::SignalGenerator(int id) : m_id(id), m_active_tone(std::make_pai
 
 void SignalGenerator::setup(const char* title, bool* p_open) {
 	if (ImGui::Begin(title, p_open)) {
+		ImGui::Checkbox("Measure", &m_measurement_active);
 		ImGui::InputInt("Tone index", &m_active_tone.first, 1, 100);
 		ImGui::InputDouble("Tone amplitude (dBm)", &m_active_tone.second, 1, 5, "%.0f");
 		ImGui::End();
