9975506616ea9f53b5ef40eefe69b366d0587129
Jaco du Preez <u086578@corp.saab.se>
2025-10-29 13:24:23 +0200

    Replace stop frequency setting with helper

--- spectrum_analyzer/src/spectrum_analyzer.cpp
+++ spectrum_analyzer/src/spectrum_analyzer.cpp
@@ -78,7 +78,7 @@ void SpectrumAnalyzer::clearTones() {
 void SpectrumAnalyzer::draw(const char* title, bool* p_open) {
 	if (ImGui::Begin(title, p_open)) {
 		inputDouble("Start Frequency (Hz)", m_start_freq, 1e6, 100e6, "%.0f", MIN_FREQ, MAX_FREQ);
-		ImGui::InputDouble("Stop Frequency (Hz)", &m_stop_freq, 1e6, 100e6, "%.0f");
+		inputDouble("Stop Frequency (Hz)", m_stop_freq, 1e6, 100e6, "%.0f", MIN_FREQ, MAX_FREQ);
 		ImGui::InputDouble("VBW (Hz)", &m_vbw, 1e6, 10e6, "%.0f");
 		ImGui::InputDouble("RBW (Hz)", &m_rbw, 1e6, 10e6, "%.0f");
 		ImGui::InputDouble("Ref (dBm)", &m_max_power, 5, 10, "%.0f");
