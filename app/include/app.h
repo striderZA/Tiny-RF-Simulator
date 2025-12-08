4cc0e068486840d83e2ee0a8c674b17dfe36ba35
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-08 14:46:26 +0200

    Remove node editor from app header

--- app/include/app.h
+++ app/include/app.h
@@ -14,43 +14,6 @@ enum class InputSignals : int {
 	G3
 };
 
-struct RfNode {
-	int id;
-	std::string name;
-	double gain_dB;
-	int inputPin;
-	int outputPin;
-};
-
-struct RfLink {
-	int id;
-	int startAttribute, endAttribute;
-};
-
-class RfNodeEditor {
-public:
-	void initialize() {
-		m_nodes.push_back({ 0, "Source", 0.0f, -1, 1 });
-		m_nodes.push_back({ 0, "Attenuator", -10.0f, 3, 4 });
-		m_nodes.push_back({ 5, "Load", 0.0f, 6, -1 });
-	}
-
-	void shutdown() {
-	}
-
-	void draw() {
-		ImNodes::BeginNodeEditor();
-		for (auto& node : m_nodes) {
-		}
-		ImNodes::EndNodeEditor();
-	}
-
-private:
-	std::vector<RfNode> m_nodes;
-	std::vector<RfLink> m_links;
-	int m_nextLinkId = 0;
-};
-
 class RfSimulatorApp {
 public:
 	RfSimulatorApp();
