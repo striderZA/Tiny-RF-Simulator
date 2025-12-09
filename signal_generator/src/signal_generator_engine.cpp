956c0f2361b966ee029b8966210b96137fa28be5
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-09 11:13:07 +0200

    Format

--- signal_generator/src/signal_generator_engine.cpp
+++ signal_generator/src/signal_generator_engine.cpp
@@ -1,3 +1,4 @@
 #include "signal_generator_engine.h"
 
-SignalGeneratorEngine::SignalGeneratorEngine(int id) : m_id(id), m_active_tone(std::make_pair<int, double>(0, 0)) {}
+SignalGeneratorEngine::SignalGeneratorEngine(int id)
+    : m_id(id), m_active_tone(std::make_pair<int, double>(0, 0)) {}
