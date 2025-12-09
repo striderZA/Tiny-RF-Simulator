b7571e73aa4d08feeb74cd39aef09b34a77d5665
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-09 15:00:54 +0200

    Format

--- logging/src/logging_core.cpp
+++ logging/src/logging_core.cpp
@@ -11,8 +11,7 @@ std::string LoggerCore::getTimestamp() const {
     auto now = std::chrono::system_clock::now();
     std::time_t now_time = std::chrono::system_clock::to_time_t(now);
     char buffer[32];
-    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S",
-                  std::localtime(&now_time));
+    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", std::localtime(&now_time));
     return buffer;
 }
 
