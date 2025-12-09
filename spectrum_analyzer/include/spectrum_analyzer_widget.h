0fa34da2cd7d4dbd07befcd0fc4f59780827f028
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-09 11:28:19 +0200

    Format

--- spectrum_analyzer/include/spectrum_analyzer_widget.h
+++ spectrum_analyzer/include/spectrum_analyzer_widget.h
@@ -2,11 +2,11 @@
 #include "spectrum_analyzer_engine.h"
 
 class SpectrumAnalyzerWidget {
-public:
-	SpectrumAnalyzerWidget(SpectrumAnalyzerEngine& engine);
+  public:
+    SpectrumAnalyzerWidget(SpectrumAnalyzerEngine &engine);
 
-	void draw(const char* title, bool* p_open = nullptr);
+    void draw(const char *title, bool *p_open = nullptr);
 
-private:
-	SpectrumAnalyzerEngine& m_engine;
+  private:
+    SpectrumAnalyzerEngine &m_engine;
 };
