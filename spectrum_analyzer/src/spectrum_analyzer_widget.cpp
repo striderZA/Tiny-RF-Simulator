3ec6f92d3a05cc115ca80de79b7c1ca4170d2029
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-09 14:38:23 +0200

    Minor formatting update

--- spectrum_analyzer/src/spectrum_analyzer_widget.cpp
+++ spectrum_analyzer/src/spectrum_analyzer_widget.cpp
@@ -5,8 +5,7 @@
 #include "utils.h"
 #include <cmath>
 
-SpectrumAnalyzerWidget::SpectrumAnalyzerWidget(SpectrumAnalyzerEngine &engine)
-    : m_engine(engine) {}
+SpectrumAnalyzerWidget::SpectrumAnalyzerWidget(SpectrumAnalyzerEngine &engine) : m_engine(engine) {}
 
 static std::vector<double> to_dBm(const std::vector<double> &input) {
     std::vector<double> out(input.size());
@@ -29,26 +28,27 @@ void SpectrumAnalyzerWidget::draw(const char *title, bool *p_open) {
     double minp = m_engine.minPower();
     double maxp = m_engine.maxPower();
 
-    if (utils::inputDouble("Start Frequency (Hz)", s, 1e6, 100e6, "%0.f",
-                           MIN_FREQ, MAX_FREQ)) {
+    if (utils::inputDouble("Start Frequency (Hz)", s, 1e6, 100e6, "%0.f", MIN_FREQ, MAX_FREQ)) {
         m_engine.setStartFrequency(s);
     }
-    if (utils::inputDouble("Stop Frequency (Hz)", e, 1e6, 100e6, "%0.f",
-                           MIN_FREQ, MAX_FREQ)) {
+
+    if (utils::inputDouble("Stop Frequency (Hz)", e, 1e6, 100e6, "%0.f", MIN_FREQ, MAX_FREQ)) {
         m_engine.setStopFrequency(e);
     }
+
     if (utils::inputDouble("VBW (Hz)", vbw, 1e6, 10e6, "%.0f", 1e6, 100e6)) {
         m_engine.setVideoBw(vbw);
     }
+
     if (utils::inputDouble("RBW (Hz)", rbw, 1e6, 10e6, "%.0f", 1e6, 100e6)) {
         m_engine.setResBw(rbw);
     }
-    if (utils::inputDouble("Ref (dBm)", maxp, 5, 10, "%.0f", MIN_POWER,
-                           MAX_POWER)) {
+
+    if (utils::inputDouble("Ref (dBm)", maxp, 5, 10, "%.0f", MIN_POWER, MAX_POWER)) {
         m_engine.setMaxPower(maxp);
     }
-    if (utils::inputDouble("Min level (dBm)", minp, 5, 10, "%.0f", MIN_POWER,
-                           MAX_POWER)) {
+
+    if (utils::inputDouble("Min level (dBm)", minp, 5, 10, "%.0f", MIN_POWER, MAX_POWER)) {
         m_engine.setMinPower(minp);
     }
 
@@ -60,9 +60,8 @@ void SpectrumAnalyzerWidget::draw(const char *title, bool *p_open) {
     const auto &spec = m_engine.spectrum();
     const auto dBm = to_dBm(spec.noise_power_W);
 
-    ImPlot::SetNextAxesLimits(m_engine.startFrequency(),
-                              m_engine.stopFrequency(), m_engine.minPower(),
-                              m_engine.maxPower(), ImPlotCond_Always);
+    ImPlot::SetNextAxesLimits(m_engine.startFrequency(), m_engine.stopFrequency(),
+                              m_engine.minPower(), m_engine.maxPower(), ImPlotCond_Always);
 
     if (ImPlot::BeginPlot("Spectrum Analyzer")) {
         ImPlot::PlotLine("Spectrum", spec.frequencies.data(), dBm.data(),
