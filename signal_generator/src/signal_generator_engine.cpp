3bbfd03e590b854636b1d57eaedfc65ce502e0b2
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-10 13:49:05 +0200

    Calculate the frequency scale from the start/stop/step

--- signal_generator/src/signal_generator_engine.cpp
+++ signal_generator/src/signal_generator_engine.cpp
@@ -7,3 +7,9 @@ SignalGeneratorEngine::SignalGeneratorEngine(int id)
     const double stop_Hz = MAX_FREQ;
     const double step_Hz = 100e6;
     int n = static_cast<int>((stop_Hz - start_Hz) / step_Hz);
+
+    m_node.output.frequencies.resize(n);
+
+    for (int i = 0; i < n; ++i) {
+        m_node.output.frequencies[i] = start_Hz + i * step_Hz;
+    }
