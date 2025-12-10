58ef663e299a5701ea597f416a604f2b1f9c0a8f
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-10 13:39:56 +0200

    Significantly reduce the functionality of the spectrum engine

--- spectrum_analyzer/include/spectrum_analyzer_engine.h
+++ spectrum_analyzer/include/spectrum_analyzer_engine.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "common.h"
+#include "spectrum.h"
 #include <random>
 #include <vector>
 
@@ -19,39 +20,21 @@ class SpectrumAnalyzerEngine {
     double stopFrequency() const { return m_stop_freq; }
     double minPower() const { return m_min_power; }
     double maxPower() const { return m_max_power; }
-    double span() const { return m_span; }
-    int numPoints() const { return m_num_points; }
     double vbw() const { return m_vbw; }
     double rbw() const { return m_rbw; }
 
-    // DSP operations
-    void updateSpectrum();
-    void updateNoiseLevel();
-
-    void addToneRef(const tone *tone_ref);
-    void removeToneRef(const tone *tone_ref);
-
-    void clearTones() { m_current_spectrum.tones.clear(); }
-
-    const Spectrum &spectrum() const { return m_current_spectrum; }
-    double noiseLevel_dBm() const { return m_noise_level_dBm; }
+    std::vector<double> renderSpectrum(const Spectrum &spec) const;
 
   private:
-    Spectrum m_current_spectrum;
-
     // Config
     double m_start_freq = MIN_FREQ;
     double m_stop_freq = MAX_FREQ;
     double m_min_power = MIN_POWER;
     double m_max_power = MAX_POWER;
-    double m_span;
-    int m_num_points;
     double m_vbw = DEFAULT_VBW;
     double m_rbw = DEFAULT_RBW;
 
-    double m_noise_level_dBm = MIN_POWER;
-
-    // Internal helpers
-    double generateNoiseSample() const;
-    std::vector<double> generateNoiseVector() const;
+    std::vector<double> integratePowerPerBin(const Spectrum &spec) const;
+    std::vector<double> applyRbw(const std::vector<double> &power_W, double bin_width) const;
+    std::vector<double> applyVbw(const std::vector<double> &power_dBm, double bin_width) const;
 };
