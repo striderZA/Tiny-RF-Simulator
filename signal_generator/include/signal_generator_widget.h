5489ff1290e39b806e6eadf158380837678aeedb
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-10 13:36:25 +0200

    Remove unnecessary measurement active member, now encapsulated in node spectrum

--- signal_generator/include/signal_generator_widget.h
+++ signal_generator/include/signal_generator_widget.h
@@ -6,9 +6,6 @@ class SignalGeneratorWidget {
     SignalGeneratorWidget(SignalGeneratorEngine &engine);
     void draw(const char *title, bool *p_open = nullptr);
 
-    bool measurementActive() const { return m_measurement_active; }
-
   private:
     SignalGeneratorEngine &m_engine;
-    bool m_measurement_active;
 };
