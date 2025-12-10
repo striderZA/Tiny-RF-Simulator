ed357609d24946347d1b782a7f6ed3181ce6afac
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-10 21:58:40 +0200

    Reduce the generator engine step size

--- signal_generator/src/signal_generator_engine.cpp
+++ signal_generator/src/signal_generator_engine.cpp
@@ -5,7 +5,7 @@ SignalGeneratorEngine::SignalGeneratorEngine(int id)
     : m_id(id), m_active_tone(std::make_pair<int, double>(0, -60.0)) {
     const double start_Hz = MIN_FREQ;
     const double stop_Hz = MAX_FREQ;
-    const double step_Hz = 100e6;
+    const double step_Hz = 10e6;
     int n = static_cast<int>((stop_Hz - start_Hz) / step_Hz);
 
     m_node.output.frequencies.resize(n);
