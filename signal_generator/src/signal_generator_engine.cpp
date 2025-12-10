7f2ca112bb1407e667d406b598470e44d1f2e5f9
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-10 13:38:40 +0200

    Change default tone power to -60dBm

--- signal_generator/src/signal_generator_engine.cpp
+++ signal_generator/src/signal_generator_engine.cpp
@@ -1,4 +1,4 @@
 #include "signal_generator_engine.h"
 
 SignalGeneratorEngine::SignalGeneratorEngine(int id)
-    : m_id(id), m_active_tone(std::make_pair<int, double>(0, 0)) {}
+    : m_id(id), m_active_tone(std::make_pair<int, double>(0, -60.0)) {
