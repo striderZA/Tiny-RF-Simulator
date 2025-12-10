8c665c6397504ae5b8e5cb40a2eca3b1001c170c
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-10 13:48:00 +0200

    Add math library for common signal calculations

--- signal_generator/src/signal_generator_engine.cpp
+++ signal_generator/src/signal_generator_engine.cpp
@@ -1,4 +1,5 @@
 #include "signal_generator_engine.h"
+#include <cmath>
 
 SignalGeneratorEngine::SignalGeneratorEngine(int id)
     : m_id(id), m_active_tone(std::make_pair<int, double>(0, -60.0)) {
