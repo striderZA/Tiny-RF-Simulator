de6acc318d8ab638a474d048a2d89b68d708e749
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-10 13:48:20 +0200

    Set some startup constants for the generator engine

--- signal_generator/src/signal_generator_engine.cpp
+++ signal_generator/src/signal_generator_engine.cpp
@@ -3,3 +3,7 @@
 
 SignalGeneratorEngine::SignalGeneratorEngine(int id)
     : m_id(id), m_active_tone(std::make_pair<int, double>(0, -60.0)) {
+    const double start_Hz = MIN_FREQ;
+    const double stop_Hz = MAX_FREQ;
+    const double step_Hz = 100e6;
+    int n = static_cast<int>((stop_Hz - start_Hz) / step_Hz);
