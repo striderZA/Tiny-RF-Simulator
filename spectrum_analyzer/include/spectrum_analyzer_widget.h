68f8f8c8751655bdab057cef8daeeee7eb4099c8
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-10 13:39:21 +0200

    Add view manager to the spectrum widget

--- spectrum_analyzer/include/spectrum_analyzer_widget.h
+++ spectrum_analyzer/include/spectrum_analyzer_widget.h
@@ -1,12 +1,13 @@
 #pragma once
 #include "spectrum_analyzer_engine.h"
-
+#include "view_manager.h"
 class SpectrumAnalyzerWidget {
   public:
-    SpectrumAnalyzerWidget(SpectrumAnalyzerEngine &engine);
+    SpectrumAnalyzerWidget(SpectrumAnalyzerEngine &engine, ViewManager &vm);
 
     void draw(const char *title, bool *p_open = nullptr);
 
   private:
     SpectrumAnalyzerEngine &m_engine;
+    ViewManager &m_view_manager;
 };
