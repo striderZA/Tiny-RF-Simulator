f32aaf6b9290d9993231de6eebbe65046aacff75
Jaco du Preez <jaco.dupreez@za.saabgroup.com>
2025-12-11 13:23:51 +0200

    Add gain and noise figure as signal generator component values

--- signal_generator/include/signal_generator_engine.h
+++ signal_generator/include/signal_generator_engine.h
@@ -12,11 +12,19 @@ class SignalGeneratorEngine {
     void setToneFrequency(double frequency) { m_active_tone.first = frequency; }
     void setToneAmplitude(double dBm) { m_active_tone.second = dBm; }
 
+    void setGain_dB(double g) { m_gain_dB = g; }
+    void setNF_dB(double nf) { m_nf_dB = nf; }
+
     SignalNode &node() { return m_node; }
     void update(double dt);
+
+    double gain_dB() const { return m_gain_dB; }
+    double nf_dB() const { return m_nf_dB; }
 
   private:
     int m_id;
     tone m_active_tone;
     SignalNode m_node;
+    double m_gain_dB = 0.0;
+    double m_nf_dB = 0.0;
 };
