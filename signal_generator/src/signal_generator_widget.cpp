6cc44755c74b00a215bc5c2938778d0a343649d9
Jaco du Preez <jaco.dupreez7@gmail.com>
2025-12-13 18:32:57 +0200

    Add control to generator widget for bin width

--- signal_generator/src/signal_generator_widget.cpp
+++ signal_generator/src/signal_generator_widget.cpp
@@ -1,6 +1,7 @@
 #include "signal_generator_widget.h"
 #include "imgui.h"
 #include "logging_core.h"
+#include "utils.h"
 
 SignalGeneratorWidget::SignalGeneratorWidget(SignalGeneratorEngine &engine) : m_engine(engine) {}
 
@@ -8,6 +9,7 @@ void SignalGeneratorWidget::draw(const char *title, bool *p_open) {
     if (ImGui::Begin(title, p_open)) {
         double tone_frequency = m_engine.activeTone().first;
         double amplitude = m_engine.activeTone().second;
+        double bin_width = m_engine.f_step_Hz();
 
         if (ImGui::Checkbox("Measure", &m_engine.node().view_enabled)) {
             LOG_INFO("Change measurement active state [gen%d -> %s].", m_engine.id(),
@@ -16,12 +18,18 @@ void SignalGeneratorWidget::draw(const char *title, bool *p_open) {
 
         if (ImGui::InputDouble("Frequency (Hz)", &tone_frequency, 1e6, 100e6)) {
             m_engine.setToneFrequency(tone_frequency);
-            LOG_INFO("Update tone frequency: [gen%d -> %.0f MHz].", m_engine.id(), tone_frequency/1e6);
+            LOG_INFO("Update tone frequency: [gen%d -> %.0f MHz].", m_engine.id(),
+                     tone_frequency / 1e6);
         }
 
         if (ImGui::InputDouble("Amplitude (dBm)", &amplitude, 1, 5, "%.0f")) {
             m_engine.setToneAmplitude(amplitude);
             LOG_INFO("Update tone amplitude: [gen%d -> %.0f dBm]", m_engine.id(), amplitude);
+        }
+
+        if (utils::inputDouble("Bin width (Hz)", bin_width, 1e6, 10e6, "%0.f", 1e6, 100e6)) {
+            m_engine.setFreqStep(bin_width);
+            LOG_INFO("Update bin width: [gen%d -> %.0f Hz]", m_engine.id(), bin_width);
         }
 
         ImGui::End();
