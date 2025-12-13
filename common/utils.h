a27c97f190eb23812469369057c845b8152ca2c2
Jaco du Preez <jaco.dupreez7@gmail.com>
2025-12-13 18:32:27 +0200

    Format, change inputDouble to static method

--- common/utils.h
+++ common/utils.h
@@ -1,25 +1,22 @@
 #pragma once
 
+#include "imgui.h"
 #include "logging_core.h"
 
 namespace utils {
-bool inputDouble(std::string label, double &ref, double minorStep,
-                 double majorStep, const char *format, double lowerLimit,
-                 double upperLimit) {
+static bool inputDouble(std::string label, double &ref, double minorStep, double majorStep,
+                        const char *format, double lowerLimit, double upperLimit) {
     // clamp external writes to ref BEFORE drawing widget
     if (ref > upperLimit) {
-        LOG_WARN("Unable to update value: %s! (above upper limit)",
-                 label.c_str());
+        LOG_WARN("Unable to update value: %s! (above upper limit)", label.c_str());
         ref = upperLimit;
     } else if (ref < lowerLimit) {
-        LOG_WARN("Unable to update value: %s! (below lower limit)",
-                 label.c_str());
+        LOG_WARN("Unable to update value: %s! (below lower limit)", label.c_str());
         ref = lowerLimit;
     }
 
     // perform ImGui update
-    bool changed =
-        ImGui::InputDouble(label.c_str(), &ref, minorStep, majorStep, format);
+    bool changed = ImGui::InputDouble(label.c_str(), &ref, minorStep, majorStep, format);
 
     // optionally clamp AFTER user change too
     if (ref > upperLimit) {
