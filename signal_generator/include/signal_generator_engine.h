992b52d285eaf1f7056a7615c3372b89f2d96182
Jaco du Preez <jaco.dupreez7@gmail.com>
2025-12-13 17:51:28 +0200

    Add helpers to set/get f_step member

--- signal_generator/include/signal_generator_engine.h
+++ signal_generator/include/signal_generator_engine.h
@@ -11,7 +11,7 @@ class SignalGeneratorEngine {
 
     void setToneFrequency(double frequency) { m_active_tone.first = frequency; }
     void setToneAmplitude(double dBm) { m_active_tone.second = dBm; }
-
+    void setFreqStep(double Hz) { m_f_step_Hz = Hz; }
     void setGain_dB(double g) { m_gain_dB = g; }
     void setNF_dB(double nf) { m_nf_dB = nf; }
 
@@ -20,6 +20,7 @@ class SignalGeneratorEngine {
 
     double gain_dB() const { return m_gain_dB; }
     double nf_dB() const { return m_nf_dB; }
+    double f_step_Hz() const { return m_f_step_Hz; }
 
   private:
     int m_id;
