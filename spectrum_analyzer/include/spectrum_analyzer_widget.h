678e3284e1a28e94a116e43d0ae7cf3c1edd9d78
Jaco du Preez <jaco.dupreez7@gmail.com>
2025-12-16 21:05:35 +0200

    Add member to keep track of peak detect state

--- spectrum_analyzer/include/spectrum_analyzer_widget.h
+++ spectrum_analyzer/include/spectrum_analyzer_widget.h
@@ -1,13 +1,18 @@
 #pragma once
 #include "spectrum_analyzer_engine.h"
 #include "view_manager.h"
+
 class SpectrumAnalyzerWidget {
   public:
     SpectrumAnalyzerWidget(SpectrumAnalyzerEngine &engine, ViewManager &vm);
 
     void draw(const char *title, bool *p_open = nullptr);
+    bool peakDetectEnabled() const { return m_enable_peak_detect; }
+    void enablePeakDetect(bool isEnabled) { m_enable_peak_detect = isEnabled; }
+
 
   private:
     SpectrumAnalyzerEngine &m_engine;
     ViewManager &m_view_manager;
+    bool m_enable_peak_detect;
 };
