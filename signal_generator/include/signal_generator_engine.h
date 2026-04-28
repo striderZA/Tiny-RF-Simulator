290922f608b0bd7e86c134ca9a8ad61e0a162682
Jaco du Preez <jaco.dupreez7@gmail.com>
2026-04-28 12:33:29 +0200

    refactor: simplify SignalGeneratorEngine to ideal source, remove gain/NF

--- signal_generator/include/signal_generator_engine.h
+++ signal_generator/include/signal_generator_engine.h
@@ -12,22 +12,16 @@ class SignalGeneratorEngine {
     void setToneFrequency(double frequency) { m_active_tone.first = frequency; }
     void setToneAmplitude(double dBm) { m_active_tone.second = dBm; }
     void setFreqStep(double Hz) { m_f_step_Hz = Hz; rebuildFrequencyGrid(); }
-    void setGain_dB(double g) { m_gain_dB = g; }
-    void setNF_dB(double nf) { m_nf_dB = nf; }
 
     SignalNode &node() { return m_node; }
     void update(double dt);
 
-    double gain_dB() const { return m_gain_dB; }
-    double nf_dB() const { return m_nf_dB; }
     double f_step_Hz() const { return m_f_step_Hz; }
 
   private:
     int m_id;
     tone m_active_tone;
     SignalNode m_node;
-    double m_gain_dB = 0.0;
-    double m_nf_dB = 0.0;
     double m_f_step_Hz = 10e6;
 
     void rebuildFrequencyGrid();
