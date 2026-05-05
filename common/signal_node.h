e2056e22fe2c0fad76040b3810ce3b8aa0f43840
Jaco du Preez <jaco.dupreez7@gmail.com>
2026-05-05 20:44:59 +0200

    feat: change SignalNode inputs to const Spectrum pointers

--- common/signal_node.h
+++ common/signal_node.h
@@ -3,7 +3,7 @@
 #include "spectrum.h"
 
 struct SignalNode {
-    std::vector<Spectrum> inputs;
+    std::vector<const Spectrum*> inputs;
     std::vector<Spectrum> outputs;
     bool view_enabled = false;
 };
