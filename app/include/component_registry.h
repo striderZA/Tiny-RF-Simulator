52c60db10f3c8da65a8e9c181710e446acf8ec70
Jaco du Preez <jaco.dupreez7@gmail.com>
2026-05-30 22:29:11 +0200

    feat: add ComponentRegistry class

---
+++ app/include/component_registry.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "component_interface.h"
+#include "node_graph_engine.h"
+#include "view_manager.h"
+#include <memory>
+#include <span>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <vector>
+
+class ComponentRegistry {
+public:
+    ComponentRegistry(NodeGraphEngine& graph, ViewManager& view);
+
+    template<typename T, typename... Args>
+    T& add(Args&&... args) {
+        auto comp = std::make_unique<T>(std::forward<Args>(args)...);
+        T* ptr = comp.get();
+        m_view.registerNode(&ptr->node());
+        m_type_index[std::type_index(typeid(T))].push_back(
+            static_cast<IComponentEngine*>(ptr));
+        m_components.push_back(std::move(comp));
+        rebuildView();
+        return *ptr;
+    }
+
+    bool remove(int graphNodeId);
+    IComponentEngine* find(int graphNodeId) const;
+    std::string hoverSummary(int graphNodeId) const;
+
+    template<typename T>
+    std::vector<T*> byType() const {
+        std::vector<T*> result;
+        auto it = m_type_index.find(std::type_index(typeid(T)));
+        if (it == m_type_index.end()) return result;
+        result.reserve(it->second.size());
+        for (auto* p : it->second)
+            result.push_back(static_cast<T*>(p));
+        return result;
+    }
+
+    std::span<IComponentEngine* const> all() const { return m_all_view; }
+    size_t size() const { return m_components.size(); }
+
+private:
+    void rebuildView();
+
+    NodeGraphEngine& m_graph;
+    ViewManager& m_view;
+    std::vector<std::unique_ptr<IComponentEngine>> m_components;
+    std::vector<IComponentEngine*> m_all_view;
+    std::unordered_map<std::type_index, std::vector<IComponentEngine*>> m_type_index;
+};
---
+++ app/src/component_registry.cpp
@@ -0,0 +1,46 @@
+#include "component_registry.h"
+#include "logging_core.h"
+#include <algorithm>
+
+ComponentRegistry::ComponentRegistry(NodeGraphEngine& graph, ViewManager& view)
+    : m_graph(graph), m_view(view) {}
+
+bool ComponentRegistry::remove(int graphNodeId) {
+    for (size_t i = 0; i < m_components.size(); ++i) {
+        if (m_components[i]->graphNodeId() == graphNodeId) {
+            IComponentEngine* comp = m_components[i].get();
+            m_view.unregisterNode(&comp->node());
+            m_graph.removeNode(graphNodeId);
+
+            auto& idx = m_type_index[std::type_index(typeid(*comp))];
+            idx.erase(std::remove(idx.begin(), idx.end(), static_cast<IComponentEngine*>(comp)), idx.end());
+
+            m_components.erase(m_components.begin() + static_cast<std::ptrdiff_t>(i));
+            rebuildView();
+            LOG_INFO("Removed component (graph node %d)", graphNodeId);
+            return true;
+        }
+    }
+    LOG_WARN("remove: graph node %d not found", graphNodeId);
+    return false;
+}
+
+IComponentEngine* ComponentRegistry::find(int graphNodeId) const {
+    for (auto& comp : m_components) {
+        if (comp->graphNodeId() == graphNodeId)
+            return comp.get();
+    }
+    return nullptr;
+}
+
+std::string ComponentRegistry::hoverSummary(int graphNodeId) const {
+    auto* comp = find(graphNodeId);
+    return comp ? comp->hoverSummary() : "";
+}
+
+void ComponentRegistry::rebuildView() {
+    m_all_view.clear();
+    m_all_view.reserve(m_components.size());
+    for (auto& comp : m_components)
+        m_all_view.push_back(comp.get());
+}
