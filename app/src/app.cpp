9138ec9502f5fd52fb90892fc0b1cc985be9a02d
Jaco du Preez <jaco.dupreez7@gmail.com>
2026-06-23 20:51:39 +0200

    debug: add more logging for unsaved dialog flag state

--- app/src/app.cpp
+++ app/src/app.cpp
@@ -520,8 +520,8 @@ void RfSimulatorApp::draw_ui() {
                 else newProject();
             }
             if (ImGui::MenuItem("Open...", "Ctrl+O")) {
-                LOG_INFO("Open clicked, m_dirty=%d", (int)m_dirty);
-                if (m_dirty) { m_pending_action = PendingAction::Open; m_show_unsaved_dialog = true; }
+                LOG_INFO("Open clicked, m_dirty=%d show=%d", (int)m_dirty, (int)m_show_unsaved_dialog);
+                if (m_dirty) { LOG_INFO("SETTING show=true"); m_pending_action = PendingAction::Open; m_show_unsaved_dialog = true; }
                 else openFileDialog();
             }
             ImGui::Separator();
